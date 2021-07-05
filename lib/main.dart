954a4f4cc6222b56398b91e2803b040f26ebcc62
baydim <baydim56@gmail.com>
2021-07-05 10:27:26 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:becek/view/detail.dart';
+import 'package:becek/view/listuser.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,34 +11,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(home: LIst());
-  }
-}
-
-class LIst extends StatefulWidget {
-  @override
-  _LIstState createState() => _LIstState();
-}
-
-class _LIstState extends State<LIst> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          for (var i = 1; i < 8; i++)
-            TextButton(
-                onPressed: () {
-                  Get.to(
-                    () => Detail(
-                      id: i,
-                    ),
-                  );
-                },
-                child: Text('Ke user $i'))
-        ],
-      ),
-    );
   }
 }
